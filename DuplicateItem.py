0decbef791b7af84594988eac88a74f44707bd5d
vpslinuxinstall <shoparoundwyz@gmail.com>
2020-02-27 11:24:25 -0500

    commit

--- DuplicateItem.py
+++ DuplicateItem.py
@@ -18,12 +18,17 @@
 # Time complexity is O(n^2)
 
 def duplicate_item():
+    is_duplicate = False
     array_one = ['a', 'b', 'c']
     array_two = ['x', 'u', 'c']
     for array_one_item in array_one:
         for array_two_item in array_two:
             if array_one_item == array_two_item:
-                print("Found item")
+                is_duplicate = True
+    if is_duplicate:
+        print("Found item")
+    else:
+        print("There is no duplicated item.")
 
 
 # Create a hashMap
@@ -33,14 +38,27 @@ def duplicate_item():
 # Check hashMap['u'] is true or false
 # Check hashMap['c'] is true or false
 # Time complexity is O(n)
+
 def duplicate_element():
+    letter_dict = dict()
+    is_duplicate = False
     array_one = ['a', 'b', 'c']
     array_two = ['x', 'y', 'z']
-    
+    for array_one_item in array_one:
+        if array_one_item not in letter_dict:
+            letter_dict[array_one_item] = True
+    for array_two_item in array_two:
+        if array_two_item in letter_dict.keys():
+            is_duplicate = True
+    if is_duplicate:
+        print("Found duplicate")
+    else:
+        print("There is no duplicated item.")
 
 
 def main():
     duplicate_item()
+    duplicate_element()
 
 
 main()
